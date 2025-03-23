193c89d46bde123ff54a6a0f37981ba2e1a885dc
Joao123433 <joaosantos123433@gmail.com>
2025-03-22 21:49:06 -0300

    Deploying server

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -2,7 +2,7 @@ import { env } from "./src/env";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-	schema: "./src/db/schema/index.ts",
+	schema: "./dist/db/schema/index.ts",
 	out: "./.migrations",
 	dbCredentials: {
 		url: env.DATABASE_URL,
